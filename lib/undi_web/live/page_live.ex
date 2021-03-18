fd730dab1a0f12d15c40e9314664d0f6900008ee
Roslan Ramli <roslan@undionline.com>
2021-03-18 09:16:10 +0800

    modified page_live

--- lib/undi_web/live/page_live.ex
+++ lib/undi_web/live/page_live.ex
@@ -26,9 +26,9 @@ defmodule UndiWeb.PageLive do
   end
 
   defp search(query) do
-#    if not UndiWeb.Endpoint.config(:code_reloader) do
-#      raise "action disabled when not in development"
-#    end
+    if not UndiWeb.Endpoint.config(:code_reloader) do
+      raise "action disabled when not in development"
+    end
 
     for {app, desc, vsn} <- Application.started_applications(),
         app = to_string(app),
