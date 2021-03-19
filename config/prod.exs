e25fb276a8510796b1eece3ef90c5deb66c46386
Roslan <roslan-undi@undionline.com>
2021-03-19 15:30:15 +0000

    modified prod.exs

--- config/prod.exs
+++ config/prod.exs
@@ -10,7 +10,7 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :undi, UndiWeb.Endpoint,
-  url: [host: "undionline.com", port: 443],
+  url: [host: "www.undionline.com", port: 443],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
